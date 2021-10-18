29761e14703df5792485a4df999200fb8721563a
s7025311 <85991000+s7025311@users.noreply.github.com>
2021-10-18 18:59:39 +0800

    get current position in google map

--- app/src/main/java/com/tsai/shakeit/MainActivity.kt
+++ app/src/main/java/com/tsai/shakeit/MainActivity.kt
@@ -1,21 +1,18 @@
 package com.tsai.shakeit
 
 import android.os.Bundle
-import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.permissionx.guolindev.PermissionX
-import com.permissionx.guolindev.dialog.permissionMapOnQ
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.tsai.shakeit.databinding.ActivityMainBinding
-import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,50 +31,10 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        askPermission()
-
-        
-
 
     }
 
-    private fun askPermission() {
-        PermissionX.init(this)
-            .permissions(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.CALL_PHONE,
-                android.Manifest.permission.INTERNET
-            )
-            .onExplainRequestReason { scope, deniedList ->
-                scope.showRequestReasonDialog(
-                    deniedList,
-                    "主要功能需要使用到以下權限",
-                    "確定",
-                    "取消"
-                )
-            }
-            .onForwardToSettings { scope, deniedList ->
-                scope.showForwardToSettingsDialog(
-                    deniedList,
-                    "您需要到設定頁面手動開啟權限",
-                    "確定",
-                    "取消"
-                )
-            }
-            .request { allGranted, grantedList, deniedList ->
-                if (allGranted) {
-                    Toast.makeText(this, "所有權限已開啟", Toast.LENGTH_LONG).show()
-
-                } else {
-                    Toast.makeText(
-                        this,
-                        "These permissions are denied: $deniedList",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }
-    }
 }

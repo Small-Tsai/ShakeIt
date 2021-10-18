26703a618ac393fe795aac43f5b08305d0889b1c
s7025311 <85991000+s7025311@users.noreply.github.com>
2021-10-18 13:35:47 +0800

    add function to ask user permissions

--- app/src/main/java/com/tsai/shakeit/MainActivity.kt
+++ app/src/main/java/com/tsai/shakeit/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.tsai.shakeit
 
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.permissionx.guolindev.PermissionX
+import com.permissionx.guolindev.dialog.permissionMapOnQ
 import com.tsai.shakeit.databinding.ActivityMainBinding
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,8 +34,45 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        askPermission()
+    }
+
+    private fun askPermission() {
+        PermissionX.init(this)
+            .permissions(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.CALL_PHONE,
+                android.Manifest.permission.INTERNET
+            )
+            .onExplainRequestReason { scope, deniedList ->
+                scope.showRequestReasonDialog(
+                    deniedList,
+                    "Core fundamental are based on these permissions",
+                    "OK",
+                    "Cancel"
+                )
+            }
+            .onForwardToSettings { scope, deniedList ->
+                scope.showForwardToSettingsDialog(
+                    deniedList,
+                    "You need to allow necessary permissions in Settings manually",
+                    "OK",
+                    "Cancel"
+                )
+            }
+            .request { allGranted, grantedList, deniedList ->
+                if (allGranted) {
+                    Toast.makeText(this, "All permissions are granted", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "These permissions are denied: $deniedList",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 }
